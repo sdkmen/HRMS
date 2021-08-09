a2398bfd05a6cda1987daf22dd3d69ae5c49a175
Seyfullah <sdkmenn@gmail.com>
2021-08-09 14:25:32 +0300

    first commit

---
+++ Hrms/src/main/java/kodlamaio/Hrms/business/abstracts/JobPositionService.java
@@ -0,0 +1,9 @@
+package kodlamaio.Hrms.business.abstracts;
+
+import java.util.List;
+
+import kodlamaio.Hrms.entities.concretes.JobPosition;
+
+public interface JobPositionService {
+	List<JobPosition> getTitleAll();
+}
---
+++ Hrms/src/main/java/kodlamaio/Hrms/business/concretes/JobPositionManager.java
@@ -0,0 +1,28 @@
+package kodlamaio.Hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.Hrms.business.abstracts.JobPositionService;
+import kodlamaio.Hrms.dataAccess.abstracts.JobPositionDao;
+import kodlamaio.Hrms.entities.concretes.JobPosition;
+
+@Service
+public class JobPositionManager implements JobPositionService{
+	
+	private JobPositionDao jobPositionDao;
+	
+	@Autowired
+	public JobPositionManager(JobPositionDao jobPositionDao) {
+		super();
+		this.jobPositionDao = jobPositionDao;
+	}
+
+	@Override
+	public List<JobPosition> getTitleAll() {
+		return this.jobPositionDao.findAll();
+	}
+
+}
