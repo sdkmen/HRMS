c6784c7b89a268295bca4262698d93a3126d56ba
Seyfullah Dikmen <sdkmenn@gmail.com>
2021-08-11 17:50:31 +0300

    entities added

---
+++ Hrms/src/main/java/kodlamaio/Hrms/entities/concretes/Employer.java
@@ -0,0 +1,30 @@
+package kodlamaio.Hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@EqualsAndHashCode(callSuper = false)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "employers")
+@PrimaryKeyJoinColumn(name="id")
+public class Employer extends User{
+	
+	@Column(name="company_name")
+	private String companyName;
+	
+	@Column(name="web_address")
+	private String webAddress;
+	
+	@Column(name="phone_number")
+	private String phoneNumber;
+}
