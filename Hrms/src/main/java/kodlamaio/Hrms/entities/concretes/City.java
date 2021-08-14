d2bfd52387bd56763833252ada30a40813755bcf
Seyfullah Dikmen <sdkmenn@gmail.com>
2021-08-14 14:07:46 +0300

    city and jobadvert entities added

---
+++ Hrms/src/main/java/kodlamaio/Hrms/entities/concretes/City.java
@@ -0,0 +1,31 @@
+package kodlamaio.Hrms.entities.concretes;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="cities")
+public class City {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="city_name")
+	private String city_name;
+	
+}
